334b2a86ba2f25a95c9ef3ceb15f8ab2bbc2e116
praakhya <67508200+praakhya@users.noreply.github.com>
2022-05-17 12:45:18 +0530

    fixed convertor

--- conversions.c
+++ conversions.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-long binToDec(long bn, int b){
+long binOctToDec(long bn, int b){
     long temp=bn, dn=0;
     int n=0;
     printf("dn    temp     n\n");
     printf("%ld %ld %d\n", dn, temp, n);
     while (temp>0){
-        dn+=pow(b, n)*(temp%10);
-        printf("%ld %ld %d\n", dn, temp, n);
-        temp/=10;
-        n+=1;
+        int dig=temp%10;
+        if (dig<b){
+            dn+=pow(b, n)*(dig);
+            printf("%ld %ld %d\n", dn, temp, n);
+            temp/=10;
+            n+=1;
+        }
+        else {
+            return -1;
+        }
+        
         
     }
     return (long) dn;
@@ -18,48 +25,44 @@ long binToDec(long bn, int b){
 long hexToDec(char* hn){
     long dn=0;
     int n=0, len=strlen(hn);
-    printf("%10s    %3s     %3s\n","dn","hn[n]","n");
-    printf("%10ld    %3c     %3d\n",dn,hn[n],n);
     while (n<len){
-        int dig;
-        
-        if (hn[len-n-1]=='F'){
-            dig=pow(16, n)*15;
+        int dig, digA=10, dig0=48;
+        if ((hn[len-n-1]>=65)&&(hn[len-n-1]<=70)){
+            dig=digA+(hn[len-n-1]-'A');
         }
-        else if (hn[len-n-1]=='E'){
-            dig=pow(16, n)*14;
+        else if((hn[len-n-1]>=48)&&(hn[len-n-1]<=57)) {
+            dig= hn[len-n-1]-'0';
         }
-        else if (hn[len-n-1]=='D'){
-            dig=pow(16, n)*13;
-        }
-        else if (hn[len-n-1]=='C'){
-            dig=pow(16, n)*12;
-        }
-        else if (hn[len-n-1]=='B'){
-            dig=pow(16, n)*11;
-        }
-        else if (hn[len-n-1]=='A'){
-            dig=pow(16, n)*10;
-        }
-        else {
-            dig=pow(16, n)*hn[len-n-1];
-        }
-        printf("%10ld    %3c     %3d  %3.2f   %3d\n",dn,hn[len-n-1],n, pow(16, n),dig);
-        dn+=dig;
+        printf("char: %c, dig: %d\n",hn[len-n-1], dig);
+        dn+=pow(16, n)*dig;
         ++n;
     }
     return dn;
 }
-void convertor(int type){
-    if (type==1){
-        char n[100];
-        scanf("%s",n);
-        printf("\n%ld\n",hexToDec(n));
+
+
+void convertor(int base){
+    if (base==2){
+        long n;
+        //scanf("%ld",&n);
+        n=111000;
+        printf("\n%ld (Binary) ---> %ld (Decimal)\n",n,binOctToDec(n,2));
+    }
+    else if (base==8){
+        long n;
+        //scanf("%ld",&n);
+        n=78;
+        printf("\n%ld (Octal) ---> %ld (Decimal)\n",n,binOctToDec(n,8));
+    }
+    else if (base==16){
+        char n[100]="FFF222";
+        //scanf("%s",n);
+        printf("\n%s (Hex) ---> %ld (Decimal)\n",n,hexToDec(n));
     }
 }
 
 int main(){
-    long (*fun_ptr)(long,int);
-    fun_ptr = &binToDec; 
-    convertor(1);
+    convertor(2);
+    convertor(8);
+    convertor(16);
 }
